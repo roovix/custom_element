12f090e20792fcff494c64e950b9d6a0a0a8ab59
Roovix <yourtom936@gmail.com>
2025-03-25 15:59:19 +0600

    Create upload_image_to_cloudinary.js

---
+++ upload_image_to_cloudinary.js
@@ -0,0 +1,21 @@
+// Function to upload and get image url
+async function uploadImage(file) {
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', 'image_preset');
+    formData.append('folder', 'profile');
+    formData.append('api_key', '391989329564552');
+
+    try {
+        const response = await fetch('https://api.cloudinary.com/v1_1/dtmbjcbvi/image/upload', {
+            method: 'POST',
+            body: formData
+        });
+        const data = await response.json();
+        console.log(data);
+        return data.secure_url;
+    } catch (error) {
+        console.error('Upload error:', error);
+        throw error;
+    }
+}
